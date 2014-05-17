0acaa33e98793e35359889ce3d01850814b86792
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-17 09:25:44 -0700

    open / close add new list on board show view

--- app/assets/javascripts/views/new_list.js
+++ app/assets/javascripts/views/new_list.js
@@ -1,21 +1,33 @@
 Trellino.Views.NewList = Backbone.View.extend({
-  template: JST['lists/new'],
-
-  events: {
-    'submit #new-list' : 'submit'
+  template: function () {
+    return (this.open ? JST['lists/new'] : JST['lists/newadd']);
   },
 
   initialize: function (options) {
+    this.open = false;
+
     this.board = options.board;
+
+    this.listenTo(this.board.lists(), 'add', this.render);
+  },
+
+  events: {
+    'submit #new-list' : 'submit',
+    'click button' : 'openNew'
   },
 
   render: function() {
-    var renderedContent = this.template({
+    var renderedContent = this.template()({
       board: this.board
     });
 
     this.$el.html(renderedContent);
     return this;
+  },
+
+  openNew: function() {
+    this.open = true;
+    this.render();
   },
 
   submit: function(event) {
@@ -30,6 +42,7 @@ Trellino.Views.NewList = Backbone.View.extend({
 
     list.save({}, {
       success: function(resp) {
+        view.open = false;
         view.board.lists().add(list);
         view.$('.user-input').val("");
         console.log('list added');
