d59b7d6e94d1126ad1298c4e263dc6ccc72cfbf6
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-19 12:05:02 -0700

    card validation improved, still no ui messages

--- app/assets/javascripts/views/new_card.js
+++ app/assets/javascripts/views/new_card.js
@@ -6,6 +6,8 @@ Trellino.Views.NewCard = Backbone.View.extend({
   initialize: function (options) {
     this.list = options.list;
     this.open = false;
+    this.card = new Trellino.Models.Card();
+    this.listenTo(this.card, 'all', this.invalidInput)
   },
 
   events: {
@@ -14,10 +16,14 @@ Trellino.Views.NewCard = Backbone.View.extend({
     'click .close-btn': 'closeForm'
   },
 
+  invalidInput: function () {
+    console.log('hola');
+  },
+
   render: function () {
     var renderedContent = this.template()({
-      model: new Trellino.Models.Card(),
-      newRank: 9999
+      model: this.card,
+      newRank: 9999   // TODO: change this to one greater than the last card rank
     });
     this.$el.html(renderedContent);
     return this;
@@ -40,15 +46,20 @@ Trellino.Views.NewCard = Backbone.View.extend({
     var view = this;
     var list = this.list;
     $target = $(event.currentTarget);
-    var newCard = new Trellino.Models.Card($target.serializeJSON()['card']);
-    newCard.set('list_id', list.id);
-    newCard.save([], {
+    this.card.set($target.serializeJSON().card);
+    this.card.set('list_id', list.id);
+    var card = this.card;
+    card.save([], {
       // TODO this is hacky - rework it
       url: "api/lists/" + list.id + "/cards",
+      method: 'post',
       success: function () {
         console.log('card saved successfully to DB');
         $target.find('input.card-title').val('');
-        list.cards().add(newCard);
+        list.cards().add(card);
+        view.card = new Trellino.Models.Card();
+        // TODO: don't like that we have to re-add this:
+        view.listenTo(view.card, 'invalid', view.invalidInput);
         view.open = false;
         view.render();
       },
